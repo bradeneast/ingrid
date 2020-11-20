030524e873c3514b8c1b6270f972d092726e7b66
Braden East <bradeneastdesign@gmail.com>
2020-11-20 09:28:11 -0600

    make build function available as export from index.js

--- index.js
+++ index.js
@@ -3,47 +3,53 @@
 const fs = require('fs-extra');
 const path = require('path');
 
-const { dist, src, ignorePattern } = require('./lib/options.js');
+const options = require('./lib/options.js');
 const startDevServer = require('./lib/dev/server.js');
 const makeTree = require('./lib/makeTree.js');
 const render = require('./lib/render.js');
 
 
+async function buildFromSrc({ dist, src, ignorePattern }) {
 
-console.time('Built in');
-console.log('\nStarting...\n');
+	console.time('Built in');
+	console.log('\nStarting...\n');
 
-fs.ensureDirSync(dist);
-fs.ensureDirSync(src);
+	fs.ensureDirSync(dist);
+	fs.ensureDirSync(src);
 
-let dev = /-dev/i.test(process.argv.toString());
+	let dev = /-dev/i.test(process.argv.toString());
 
 
-if (dev) {
-	startDevServer({
-		port: 3000,
-		hostname: '127.0.0.1'
-	});
+	if (dev) {
+		startDevServer({
+			port: 3000,
+			hostname: '127.0.0.1'
+		});
+	}
+
+
+	if (!dev) {
+		// Clear the dist directory
+		for (let filename of fs.readdirSync(dist))
+			if (!ignorePattern.test(filename))
+				fs.removeSync(path.join(dist, filename));
+
+		// Render tree
+		let tree = makeTree();
+
+		tree.map(page => {
+			let { destination, content } = render(page, tree);
+			fs.ensureFileSync(destination);
+			fs.writeFile(destination, content);
+		})
+	}
+
+
+
+	console.timeEnd('Built in');
+	console.log('\n\n');
 }
 
 
-if (!dev) {
-	// Clear the dist directory
-	for (let filename of fs.readdirSync(dist))
-		if (!ignorePattern.test(filename))
-			fs.removeSync(path.join(dist, filename));
-
-	// Render tree
-	let tree = makeTree();
-
-	tree.map(page => {
-		let { destination, content } = render(page, tree);
-		fs.ensureFileSync(destination);
-		fs.writeFile(destination, content);
-	})
-}
-
-
-
-console.timeEnd('Built in');
-console.log('\n\n');
+buildFromSrc(options);
+module.exports = buildFromSrc;
